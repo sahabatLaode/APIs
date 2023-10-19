724d298b20f6a84b551a52faf83e401cc670a122
oifat <120258433+sahabatLaode@users.noreply.github.com>
2023-10-19 11:27:07 +0700

    p

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,13 @@ use App\Http\Controllers\Api\UserController;
 */
 Route::post('/registrasi', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+Route::apiResource('todos', TodoController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::apiResource('todos', TodoController::class);
     Route::get('/logout', [UserController::class, 'logout']);
 });
 
