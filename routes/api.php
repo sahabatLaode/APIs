72240bd2cd06534aa4f13413a7957c9cfce0be24
younot34 <94425967+younot34@users.noreply.github.com>
2023-10-31 23:12:03 +0700

    ppp

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\TodoController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ZakatController;
 use App\Http\Controllers\Api\rekController;
-use App\Http\Controllers\Api\BuktiController;
-use App\Http\Controllers\Api\SedekahController;
-use App\Http\Controllers\Api\InfaqController;
 use App\Http\Controllers\Api\InfaqFormController;
 use App\Http\Controllers\Api\SedekahFormController;
 
@@ -32,12 +28,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/logout', [UserController::class, 'logout']);
 });
-Route::apiResource('todos', TodoController::class);
+
 Route::apiResource('zakat', ZakatController::class);
 Route::apiResource('infaqform', InfaqFormController::class);
 Route::apiResource('sedekahform', SedekahFormController::class);
 Route::post('rekening', [rekController::class, 'store'])->name('rekening.store');
-Route::post('bukti', [buktiController::class, 'store'])->name('bukti.store');
-Route::post('sedekah', [SedekahController::class, 'store'])->name('sedekah.store');
-Route::post('infaq', [InfaqController::class, 'store'])->name('infaq.store');
+
 
